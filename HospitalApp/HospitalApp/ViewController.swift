01eb37725d59db54fb18a8709b642b6c23de6971
ReplaySyndrome <kimjunhyok@naver.com>
2021-05-24 14:55:55 +0900

    fitstADd

---
+++ HospitalApp/HospitalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HospitalApp
+//
+//  Created by kpugame on 2021/05/24.
+//  Copyright © 2021 junhyeok.2021smartphone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
